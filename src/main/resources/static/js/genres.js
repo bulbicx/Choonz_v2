9b05e6c43b2f26cd0f65d41c8ff458a485b7f540
Marco Castellana <arkanoidmark96@gmail.com>
2021-09-08 12:22:37 +0100

    MC: added search for genres

--- src/main/resources/static/js/genres.js
+++ src/main/resources/static/js/genres.js
@@ -1,4 +1,23 @@
 (() => {
+    let searchInput = document.querySelector("#searchInput");
+    searchInput.addEventListener("keyup", () => searchGenres());
+
+    function searchGenres() {
+        let txtValue;
+        let filter = searchInput.value.toUpperCase();
+        let card = document.getElementsByClassName("card-box");
+
+        for (let i = 0; i < card.length; i++) {
+            let p = card[i].getElementsByClassName("title-card")[0];
+            txtValue = p.textContent || p.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                card[i].style.display = "";
+            } else {
+                card[i].style.display = "none";
+            }
+        }
+    }
+
     function getGenreSinglePage(genreId) {
         fetch(`http://localhost:8082/genresingle`)
                 .then(response => response.text())
@@ -29,8 +48,7 @@
         let cardGroup = document.querySelector("#card-group");
 
         let card = document.createElement("div");
-        card.setAttribute("class", "card mb-3");
-        card.setAttribute("style", "width:12rem");
+        card.setAttribute("class", "card-box");
         card.onclick = () => {
             getGenreSinglePage(genre.id);
         }
@@ -41,15 +59,15 @@
         cardImage.setAttribute("alt", genre.image.name);
         cardImage.setAttribute("src", "data:image/" + genre.image.type + ";base64," + genre.image.picByte);
         card.appendChild(cardImage);
+        
+        let imageOverlay = document.createElement("div");
+        imageOverlay.setAttribute("class", "image_overlay");
+        card.appendChild(imageOverlay);
 
-        let cardBody = document.createElement("div");
-        cardBody.setAttribute("class", "card-body");
-        card.appendChild(cardBody);
-
-        let cardBodyText = document.createElement("div");
-        cardBodyText.setAttribute("class", "text-playlist-card");
-        cardBodyText.textContent = genre.name;
-        cardBody.appendChild(cardBodyText);
+        let titleCard = document.createElement("p");
+        titleCard.setAttribute("class", "title-card");
+        titleCard.innerText = genre.name;
+        imageOverlay.appendChild(titleCard);
     }
 }
 )();
