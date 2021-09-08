e50507111aa8700d294c10fdecd3cef24d4c2e7a
Marco Castellana <arkanoidmark96@gmail.com>
2021-09-08 12:30:55 +0100

    MC: added search on playlist

--- src/main/resources/static/js/playlists.js
+++ src/main/resources/static/js/playlists.js
@@ -7,6 +7,24 @@
   let deleteBtn = document.querySelector(".delete");
   let userId;
   let playlistId;
+  let searchInput = document.querySelector("#searchInput");
+  searchInput.addEventListener("keyup", () => searchPlaylists());
+
+  function searchPlaylists() {
+      let txtValue;
+      let filter = searchInput.value.toUpperCase();
+      let card = document.getElementsByClassName("card-box");
+
+      for (let i = 0; i < card.length; i++) {
+          let p = card[i].getElementsByClassName("title-card")[0];
+          txtValue = p.textContent || p.innerText;
+          if (txtValue.toUpperCase().indexOf(filter) > -1) {
+              card[i].style.display = "";
+          } else {
+              card[i].style.display = "none";
+          }
+      }
+  }
 
   const getAllPlaylists = () => {
     fetch(`http://localhost:8082/playlists/read`)
@@ -231,8 +249,7 @@
       main.setAttribute("class", "row main-section");
       for (let i = 0; i < data.length; i++) {
         let card = document.createElement("div");
-        card.setAttribute("class", "card p-0");
-        card.setAttribute("style", "width: 13rem");
+        card.setAttribute("class", "card-box");
 
         let img = document.createElement("img");
         img.setAttribute("alt", data[i].artwork.name);
@@ -240,10 +257,14 @@
         img.setAttribute("class", "card-img-top card-background");
         card.appendChild(img);
         
-        let p = document.createElement("p");
-        p.setAttribute("class", "card-text text");
-        p.innerText = data[i].name;
-        card.appendChild(p);
+        let imageOverlay = document.createElement("div");
+        imageOverlay.setAttribute("class", "image_overlay");
+        card.appendChild(imageOverlay);
+
+        let titleCard = document.createElement("p");
+        titleCard.setAttribute("class", "title-card");
+        titleCard.innerText = data[i].name;
+        imageOverlay.appendChild(titleCard);
         card.addEventListener("click", () => getPlaylistSinglePage(data[i].id));
         main.appendChild(card);
       }
